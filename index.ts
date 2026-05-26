1e75c74accebbf5a24224d813e3450fe81da00de
root <root@Clancy.localdomain>
2026-05-27 02:16:50 +0800

    新增类别更新功能，修复多次分类的 bug

--- index.ts
+++ index.ts
@@ -51,7 +51,7 @@ class TrainingCategoryModel {
 
     static async edit(domainId: string, cid: ObjectId, category: string, displayName: string): Promise<number> {
         const result = await TrainingCategoryModel.coll.updateOne(
-            { _id: cid },
+            { domainId, _id: cid },
             { $set: { category, displayName } }
         );
         return result.modifiedCount;
@@ -66,6 +66,14 @@ class TrainingCategoryModel {
         const result = await TrainingCategoryModel.coll.updateOne(
             { domainId, category },
             { $push: { trainingIds: trainingId } } 
+        );
+        return result;
+    }
+
+    static async dec(domainId: string, category: string, trainingId: ObjectId) {
+        const result = await TrainingCategoryModel.coll.updateOne(
+            { domainId, category },
+            { $pull: { trainingIds: trainingId } } 
         );
         return result;
     }
@@ -85,19 +93,39 @@ class TrainingCategoryHandler extends Handler {
         if (!this.user.hasPriv(PRIV.PRIV_USER_PROFILE)) {
             throw new ForbiddenError();
         }
+
         const cursor = await TrainingCategoryModel.getMulti(domainId);
         const ddocs = (await cursor.toArray()).toSorted((a, b) => a.category.localeCompare(b.category));
 
         const ddoc = await TrainingCategoryModel.getByName(domainId, category);
-
         const tdocs = ddoc.trainingIds && ddoc.trainingIds.length > 0
                     ? await TrainingModel.getMulti(domainId, { docId: { $in: ddoc.trainingIds } }).toArray()
                     : [];
+        const tids: Set<ObjectId> = new Set();
+        for (const tdoc of tdocs) tids.add(tdoc.docId);
 
-        this.response.body = {
-            ddoc, ddocs, tdocs, category,
-        };
+        const tsdict = {};
+        let tdict = {};
+        if (this.user.hasPriv(PRIV.PRIV_USER_PROFILE)) {
+            const enrolledTids: Set<ObjectId> = new Set();
+            const tsdocs = await TrainingModel.getMultiStatus(domainId, {
+                uid: this.user._id,
+                $and: [{ docId: { $in: Array.from(tids) } }, { enroll: 1 }],
+            }).toArray();
+            for (const tsdoc of tsdocs) {
+                tsdict[tsdoc.docId] = tsdoc;
+                enrolledTids.add(tsdoc.docId);
+            }
+            for (const tid of tids) enrolledTids.delete(tid);
+            if (enrolledTids.size) {
+                tdict = await TrainingModel.getList(domainId, Array.from(enrolledTids));
+            }
+        }
+        for (const tdoc of tdocs) tdict[tdoc.docId.toHexString()] = tdoc;
         this.response.template = 'training_main.html';
+        this.response.body = {
+            tdocs, tsdict, tdict, category, ddocs,
+        };
     }
 }
 
@@ -133,6 +161,7 @@ class TrainingCategoryEditHandler extends Handler {
         this.response.redirect = this.url('training_category_edit');
     }
 
+    // 更新类别
     @param('cid', Types.ObjectId)
     @param('category', Types.String)
     @param('displayName', Types.String)
@@ -179,6 +208,14 @@ class TrainingCategoryEditHandler extends Handler {
             throw new ForbiddenError();
         }
 
+        const cursor = await TrainingCategoryModel.getMulti(domainId);
+        const ddocs = (await cursor.toArray()).toSorted((a, b) => a.category.localeCompare(b.category));
+        for (const ddoc of ddocs) {
+            if (ddoc.trainingIds && ddoc.trainingIds.includes(trainingId)) {
+                await TrainingCategoryModel.dec(domainId, ddoc.category, trainingId);
+            }
+        }
+
         await TrainingCategoryModel.inc(domainId, category, trainingId);
         this.response.redirect = this.url('training_category_edit');
     }
@@ -204,14 +241,16 @@ export async function apply(ctx: Context) {
         'Manage Training Category': '管理训练类别',
         'View Training Plan': '查看训练计划',
         'Create Category': '新增类别',
-        'All Category': '所有类别',
+        'Update Category': '更新类别',
         'Delete Category': '删除类别',
         'Category': '分类',
         'No category available': '没有可用类别',
         'No training plans available': '没有可用训练',
         'Distribute Training Plans': '训练分类',
         'Click the button below to view training plans of specific category.': '点击下面的按钮即可查看该类别下的训练计划。',
+        'Category ID must be unique, it is recommended to use a valid identifier. The training homepage will display categories in dictionary order based on their IDs.': '类别 ID 需唯一，建议使用合法标识符，训练首页展示时会按照类别 ID 字典序展示。',
+        'Set category tags for training problem sets. To change the category, just recategorize.': '给训练题单设置类别标签，如需更改类别，只需要重新分类即可。',
         'Deleting a category will unclassify the training lists within it.': '删除类别后，该类别中的训练计划将处于未分类状态。',
-        'To update the category ID or display name, please delete the original category and add it again.': '如需更新类别 ID 或展示名，请删除原类别后重新新增。',
+        'You can update the category ID or display name, or both.': '可以更新类别 ID 或展示名，也可以同时更新。',
     });
 }
